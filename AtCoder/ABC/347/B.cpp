38fa4f75b669cae7d8eccd8dec5ca8c333153fad
Ishi <syi378039@gmail.com>
2024-05-15 23:36:42 +0900

    add : ABC347

---
+++ AtCoder/ABC/347/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  set<string> st;
+  for(int i = 0; i < s.size(); i++)
+    for(int j = 1; i + j <= s.size(); j++)
+      st.insert(s.substr(i, j));
+
+  cout << st.size() << endl;
+  // for (auto itr = st.begin(); itr != st.end(); ++itr)
+  //   cout << *itr << endl;
+}
---
+++ AtCoder/ABC/347/README.md
@@ -0,0 +1,15 @@
+# AtCoder ABC347
+
+## Overview
+
+[AtCoder ABC347](https://atcoder.jp/contests/abc347)を解いてみた。
+
+| Project | Result |   Date    |
+| :-----: | :----: | :-------: |
+|    A    |   AC   | 2024.5.15 |
+|    B    |   AC   | 2024.5.15 |
+|    C    |        |           |
+|    D    |        |           |
+|    E    |        |           |
+
+##

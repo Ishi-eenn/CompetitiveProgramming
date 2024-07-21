277f829493117bf0b5ac8dafbf1d84bf7047795c
Ishi <syi378039@gmail.com>
2024-07-21 21:03:19 +0900

    add : Library Palindrome

--- AtCoder/Library/string/Palindrome.cpp
+++ AtCoder/Library/string/Palindrome.cpp
@@ -2,26 +2,6 @@
 using namespace std;
 
 /**
- * @brief 文字列の特定の部分が回文であるかどうかを判定する関数
- *
- * 指定された開始インデックスと終了インデックスの間の部分文字列が回文かどうかをチェックする。
- *
- * @param str チェックする入力文字列
- * @param start 部分文字列の開始インデックス
- * @param end 部分文字列の終了インデックス
- * @return 部分文字列が回文であればtrue、そうでなければfalseを返す
- */
-bool isSubstringPalindrome(const string& str, int start, int end) {
-  while (start < end) {
-    if (str[start] != str[end])
-      return false;
-    start++;
-    end--;
-  }
-  return true;
-}
-
-/**
  * @brief 文字列に長さKの回文が含まれているかをチェックする。
  *
  * 与えられた文字列の中に、指定された長さKの回文が含まれているかどうかを確認。
@@ -30,14 +10,13 @@ bool isSubstringPalindrome(const string& str, int start, int end) {
  * @param K チェックする回文の長さ。
  * @return 長さKの回文が含まれていればtrue、そうでなければfalseを返す。
  */
-bool containsPalindromeOfLengthK(const string& str, int K) {
-  int n = str.length();
-  if (K > n) return false;
-
-  for (int i = 0; i <= n - K; i++) {
-    if (isSubstringPalindrome(str, i, i + K - 1)) {
+bool hasPalindromeSubstring(const string& str, int k) {
+  for (int i = 0; i < str.size() - k + 1; i++) {
+    string t = str.substr(i, k);
+    string rt = t;
+    reverse(rt.begin(), rt.end());
+    if (t == rt)
       return true;
-    }
   }
   return false;
 }
@@ -51,7 +30,7 @@ bool containsPalindromeOfLengthK(const string& str, int K) {
  * @return 文字列全体が回文であればtrue、そうでなければfalseを返す。
  */
 bool isPalindrome(const string& str) {
-  return isSubstringPalindrome(str, 0, str.length() - 1);
+    return hasPalindromeSubstring(str, str.length());
 }
 
 /**

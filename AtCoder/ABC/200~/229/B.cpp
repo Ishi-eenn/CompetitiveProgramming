341370bde70b660af9864dcbb525c50ae7b30f18
Ishi <syi378039@gmail.com>
2024-08-13 07:40:20 +0900

    add : ABC B

--- AtCoder/ABC/200~/229/B.cpp
+++ AtCoder/ABC/200~/229/B.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#define int ll
+#define ll long long
 
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  ll a, b;
+  cin >> a >> b;
+
+  while(a > 0 && b > 0) {
+    if(a % 10 + b % 10 >= 10) {
+      cout << "Hard" << endl;
+      return 0;
+    }
+    a /= 10;
+    b /= 10;
+  }
+
+  cout << "Easy" << endl;
 }

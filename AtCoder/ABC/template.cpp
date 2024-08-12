bbc05bb2bf23eaa792c58f8ef271975fd02731eb
Ishi <syi378039@gmail.com>
2024-08-13 07:40:07 +0900

    add : template.cpp

---
+++ AtCoder/ABC/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int ll
+#define ll long long
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  ll n;
+  cin >> n;
+
+}

c79365fbd73388a3bcf5e6e57bc924446a27dda5
Ishi <syi378039@gmail.com>
2024-08-14 00:41:29 +0900

    テンプレートを変更

--- kyopuro-tessoku/A/template.cpp
+++ kyopuro-tessoku/A/template.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 #define int ll
 #define ll long long
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
 
 void solve() {
 

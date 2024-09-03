989754f1efbe49dd509980fedfd5b8f517644e13
Ishi <syi378039@gmail.com>
2024-09-03 22:31:22 +0900

    競プロの鉄則: 2 章：累積和（問題 A06～A10／B06～B09）

--- tessoku-book/b09/main.cpp
+++ tessoku-book/b09/main.cpp
@@ -7,6 +7,35 @@ using namespace std;
 #define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
 
 void solve() {
+  ll n, ans = 0;
+  const ll max = 1500;
+  cin >> n;
+
+  vector< vector<ll> > s(max + 2, vector<ll>(max + 2, 0));
+  for(ll i = 0; i < n; i++) {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    s[a][b]++;
+    s[a][d]--;
+    s[c][b]--;
+    s[c][d]++;
+  }
+
+  for(ll i = 0; i <= max; i++)
+    for(ll j = 1; j <= max; j++)
+      s[i][j] += s[i][j - 1];
+
+  for(ll i = 1; i <= max; i++)
+    for(ll j = 0; j <= max; j++)
+      s[i][j] += s[i - 1][j];
+
+  for(ll i = 0; i <= max; i++)
+    for(ll j = 0; j <= max; j++)
+      if(s[i][j] > 0)
+        ans++;
+
+  cout << ans << endl;
 }
 
 signed main() {

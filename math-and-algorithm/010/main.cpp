609f934dfd937b02016fc27f5089464e02dc58bb
Ishi <syi378039@gmail.com>
2024-09-04 18:04:00 +0900

    アルゴリズムと数学 : 2.5 節｜その他の基本的な数学の知識

--- math-and-algorithm/010/main.cpp
+++ math-and-algorithm/010/main.cpp
@@ -6,7 +6,16 @@ using namespace std;
 #define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
 #define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
 
+ll f(ll n) {
+  if(n == 0 || n == 1) return 1;
+  else return n * f(n - 1);
+}
+
 void solve() {
+  ll n;
+  cin >> n;
+
+  cout << f(n) << endl;
 }
 
 signed main() {
--- math-and-algorithm/011/main.cpp
+++ math-and-algorithm/011/main.cpp
@@ -6,7 +6,28 @@ using namespace std;
 #define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
 #define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
 
+ll is_prime(ll n) {
+  if(n == 2) return 1;
+  if(n % 2 == 0) return 0;
+
+  for(ll i = 3; i * i <= n; i += 2)
+    if(n % i == 0) return 0;
+
+  return 1;
+}
+
 void solve() {
+  ll n;
+  cin >> n;
+
+  vector<ll> ans;
+
+  for(ll i = 2; i <= n; i++)
+    if(is_prime(i))
+      ans.push_back(i);
+
+  for(ll i = 0; i < ans.size(); i++)
+    cout << ans[i] << " \n"[i == ans.size() - 1];
 }
 
 signed main() {
